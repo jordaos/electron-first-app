52cb6f244c78b0c314d6b4f34f98d9446b33035b
Jordão <jordao05@hotmail.com>
2018-11-15 23:38:56 +0000

    Make dashboard

--- app/js/renderer.js
+++
@@ -1,9 +0,0 @@
-const { ipcRenderer } = require('electron')
-
-console.log("I'm working");
-
-let aboutButton = document.querySelector('#about-app');
-
-aboutButton.addEventListener('click', function() {
-  ipcRenderer.send('open-about-app');
-});
