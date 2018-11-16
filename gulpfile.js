22620ab5ab634b13a9199b31850e7289a6a57cad
Jordão <jordao05@hotmail.com>
2018-11-15 22:34:41 -0200

    Add gulp file

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var runElectron = require("gulp-run-electron");
+
+gulp.task('default', ['server', 'sass:watch']);
+
+gulp.task('server', function() {
+  gulp.src("./")
+    .pipe(runElectron([], {cwd: "app"}));
+  
+  gulp.start('server:watch');
+  console.log("aaaa");
+});
+
+gulp.task('server:watch', function() {
+  gulp.watch("./app/*", ['server', runElectron.rerun]);
+});
+
+gulp.task('sass', function () {
+  gulp.src('./app/scss/sb-admin.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./app/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./app/scss/**/*.scss', ['sass']);
+});
